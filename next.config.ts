82f75a0503b8e56becdee73966d7acf79143794a
missionevangeliquesalem10-dev <missionevangeliquesalem10@gmail.com>
2025-11-28 16:59:59 +0000

    Update next.config.ts
    auto

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
+  allowedDevOrigins: [
+    "http://localhost:3000",       // Développement local
+    "http://192.168.1.75:3000",    // Remplace par ton IP locale si différente
+  ],
 };
 
 export default nextConfig;
